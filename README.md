e9b0920a728c06c22a92c3690dffd0f96984f46a
Zaldana <alexandrazaldana@gmail.com>
2019-02-24 16:30:23 -0800

    portfolio page update

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Updated-Portfolio
+# Updated Portfolio
+
+Web Application that allows users to view my personal portfolio as well as contact me with any inquiries. 
+
+
+## Description
+ 
+
+## Sample View
+
+![Portfolio Screenshot](/images/screenshot.jpg/)
+
+## Technology
+
++ Javascript
++ jQuery
++ HTML5
++ CSS3
++ Firebase
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){ 
+$(document).ready(function () {
 
 
     var config = {
@@ -8,8 +8,8 @@ $(document).ready(function(){
         projectId: "portfolio-b21ec",
         storageBucket: "portfolio-b21ec.appspot.com",
         messagingSenderId: "946041206431"
-      };
-      firebase.initializeApp(config);
+    };
+    firebase.initializeApp(config);
 
     var database = firebase.database();
 
@@ -18,11 +18,22 @@ $(document).ready(function(){
     var mainText = $(".about-me");
     var portfolio = $(".portfolio");
     var contact = $(".contact");
-    var leftSide = $(".left-side");
+    var pretzelSide = $("#block-text");
+    var brewSide = $("#block-text-01");
+    var valleySide = $("#block-text-02");
+    var alightSide = $("#block-text-03");
+    var triviaSide = $("#block-text-04");
+    var duckSide = $("#block-text-05");
+    var xySide = $("#block-text-06");
+    var comedySide = $("#block-text-07");
+    var trainSide = $("#block-text-08");
+    var gullsSide = $("#block-text-09");
     var form = $('form');
     var logo = $("#logo");
+    var pretzelPage = $(".pretzel");
+    var brewPage = $(".brew");
 
-    $("#submit").on('click', function(event){
+    $("#submit").on('click', function (event) {
         event.preventDefault();
 
         var first = $("#firstName").val().trim();
@@ -44,37 +55,48 @@ $(document).ready(function(){
         $("#message").val('');
 
         database.ref().push({
-            first:first,
-            last:last,
-            emailAddress:emailAddress,
-            subjectLine:subjectLine,
-            comment:comment,
+            first: first,
+            last: last,
+            emailAddress: emailAddress,
+            subjectLine: subjectLine,
+            comment: comment,
         })
         return false;
     })
 
-    database.ref().on("child_added", function(snap){
+    database.ref().on("child_added", function (snap) {
         var save = snap.val();
 
         console.log(snap.key, snap.val());
-    
+
         console.log(save.first);
         console.log(save.last);
         console.log(save.emailAddress);
         console.log(save.subjectLine);
         console.log(save.comment);
     })
-    
 
-    function viewPortfolio(){
+
+    function viewPortfolio() {
         var homePageButton = $("#startButton");
         mainPage.hide();
         portfolio.hide();
         contact.hide();
-        leftSide.hide();
+        pretzelSide.hide();
+        brewSide.hide();
+        valleySide.hide();
+        alightSide.hide();
+        triviaSide.hide();
+        duckSide.hide();
+        xySide.hide();
+        comedySide.hide();
+        trainSide.hide();
+        gullsSide.hide();
         form.hide();
+        pretzelPage.hide();
+        brewPage.hide();
 
-        homePageButton.on('click', function (){
+        homePageButton.on('click', function () {
             homePage.empty();
             mainPage.show();
             $("#home").css('font-weight', 'bold');
@@ -83,14 +105,25 @@ $(document).ready(function(){
 
     viewPortfolio();
 
-    function clickLogo(){
-        logo.on('click', function(){
+    function clickLogo() {
+        logo.on('click', function () {
             homePage.empty();
             mainText.show();
             portfolio.hide();
             contact.hide();
-            leftSide.hide();
+            pretzelSide.hide();
+            brewSide.hide();
+            valleySide.hide();
+            alightSide.hide();
+            triviaSide.hide();
+            duckSide.hide();
+            xySide.hide();
+            comedySide.hide();
+            trainSide.hide();
+            gullsSide.hide();
             form.hide();
+            pretzelPage.hide();
+            brewPage.hide();
             $("#home").css('font-weight', 'bold');
             $("#portfolio").css('font-weight', 'normal');
             $("#contact").css('font-weight', 'normal');
@@ -101,16 +134,27 @@ $(document).ready(function(){
 
     clickLogo();
 
-    function clickHome(){
+    function clickHome() {
         var homeButton = $("#home");
 
-        homeButton.on('click', function (){
+        homeButton.on('click', function () {
             homePage.empty();
             mainText.show();
             portfolio.hide();
             contact.hide();
-            leftSide.hide();  
+            pretzelSide.hide();
+            brewSide.hide();
+            valleySide.hide();
+            alightSide.hide();
+            triviaSide.hide();
+            duckSide.hide();
+            xySide.hide();
+            comedySide.hide();
+            trainSide.hide();
+            gullsSide.hide();
             form.hide();
+            pretzelPage.hide();
+            brewPage.hide();
             $(".graphic").css('font-weight', 'normal');
             $("#home").css('font-weight', 'bold');
             $("#portfolio").css('font-weight', 'normal');
@@ -120,16 +164,27 @@ $(document).ready(function(){
 
     clickHome();
 
-    function clickPortfolio(){
+    function clickPortfolio() {
         var portfolioButton = $("#portfolio");
 
-        portfolioButton.on('click', function (){
+        portfolioButton.on('click', function () {
             homePage.empty();
             mainText.hide();
             portfolio.show();
             contact.hide();
-            leftSide.show();
+            pretzelSide.hide();
+            brewSide.hide();
+            valleySide.hide();
+            alightSide.hide();
+            triviaSide.hide();
+            duckSide.hide();
+            xySide.hide();
+            comedySide.hide();
+            trainSide.hide();
+            gullsSide.hide();
             form.hide();
+            pretzelPage.hide();
+            brewPage.hide();
             $(".graphic").css('font-weight', 'normal');
             $("#home").css('font-weight', 'normal');
             $("#portfolio").css('font-weight', 'bold');
@@ -143,16 +198,27 @@ $(document).ready(function(){
 
     clickPortfolio();
 
-    function clickContact(){
+    function clickContact() {
         var contactButton = $("#contact");
 
-        contactButton.on('click', function (){
+        contactButton.on('click', function () {
             homePage.empty();
             mainText.hide();
             portfolio.hide();
             contact.show();
-            leftSide.hide();
+            pretzelSide.hide();
+            brewSide.hide();
+            valleySide.hide();
+            alightSide.hide();
+            triviaSide.hide();
+            duckSide.hide();
+            xySide.hide();
+            comedySide.hide();
+            trainSide.hide();
+            gullsSide.hide();
             form.hide();
+            pretzelPage.hide();
+            brewPage.hide();
             $(".graphic").css('font-weight', 'normal');
             $("#home").css('font-weight', 'normal');
             $("#contact").css('font-weight', 'bold');
@@ -162,31 +228,245 @@ $(document).ready(function(){
 
     clickContact();
 
-    function sendEmail(){
+    function sendEmail() {
         var emailButton = $("#emailButton");
 
-        emailButton.on('click', function (){
+        emailButton.on('click', function () {
             homePage.empty();
             mainPage.show();
             form.show();
             contact.hide();
+            pretzelPage.hide();
+            pretzelSide.hide();
+            brewSide.hide();
+            valleySide.hide();
+            alightSide.hide();
+            triviaSide.hide();
+            duckSide.hide();
+            xySide.hide();
+            comedySide.hide();
+            trainSide.hide();
+            gullsSide.hide();
+            brewPage.hide();
             $("#home").css('font-weight', 'normal');
         });
     }
 
     $('#myModal').on('shown.bs.modal', function () {
         $('#myInput').trigger('focus')
-      })
+    })
 
     sendEmail();
 
-    // function submitEmail(){
-    //     var submit = $("#submit");
+    function pretzel() {
+        var pretzelClicked = $(".project01");
 
-    
-    // }
+        pretzelClicked.on('click', function () {
+            portfolio.hide();
+            pretzelPage.show();
+            pretzelSide.show();
+            brewSide.hide();
+            valleySide.hide();
+            alightSide.hide();
+            triviaSide.hide();
+            duckSide.hide();
+            xySide.hide();
+            comedySide.hide();
+            trainSide.hide();
+            gullsSide.hide();
+            brewPage.hide();
+        })
+    }
+    pretzel();
 
-    // submitEmail();
+    function brew() {
+        var brewClicked = $(".project02");
+
+        brewClicked.on('click', function () {
+            portfolio.hide();
+            pretzelPage.hide();
+            pretzelSide.hide();
+            brewSide.show();
+            valleySide.hide();
+            alightSide.hide();
+            triviaSide.hide();
+            duckSide.hide();
+            xySide.hide();
+            comedySide.hide();
+            trainSide.hide();
+            gullsSide.hide();
+            brewPage.show();
+        })
+    }
+    brew();
+
+    function valleyView() {
+        var valleyClicked = $(".project03");
+
+        valleyClicked.on('click', function () {
+            portfolio.hide();
+            pretzelPage.hide();
+            pretzelSide.hide();
+            brewSide.hide();
+            valleySide.show();
+            alightSide.hide();
+            triviaSide.hide();
+            duckSide.hide();
+            xySide.hide();
+            comedySide.hide();
+            trainSide.hide();
+            gullsSide.hide();
+            brewPage.hide();
+        })
+    }
+    valleyView();
+
+    function alight() {
+        var alightClicked = $(".project04");
+
+        alightClicked.on('click', function () {
+            portfolio.hide();
+            pretzelPage.hide();
+            pretzelSide.hide();
+            brewSide.hide();
+            valleySide.hide();
+            alightSide.show();
+            triviaSide.hide();
+            duckSide.hide();
+            xySide.hide();
+            comedySide.hide();
+            trainSide.hide();
+            gullsSide.hide();
+            brewPage.hide();
+        })
+    }
+    alight();
+
+    function trivia() {
+        var triviaClicked = $(".project05");
+
+        triviaClicked.on('click', function () {
+            portfolio.hide();
+            pretzelPage.hide();
+            pretzelSide.hide();
+            brewSide.hide();
+            valleySide.hide();
+            alightSide.hide();
+            triviaSide.show();
+            duckSide.hide();
+            xySide.hide();
+            comedySide.hide();
+            trainSide.hide();
+            gullsSide.hide();
+            brewPage.hide();
+        })
+    }
+    trivia();
+
+    function duckDive() {
+        var duckClicked = $(".project06");
+
+        duckClicked.on('click', function () {
+            portfolio.hide();
+            pretzelPage.hide();
+            pretzelSide.hide();
+            brewSide.hide();
+            valleySide.hide();
+            alightSide.hide();
+            triviaSide.hide();
+            duckSide.show();
+            xySide.hide();
+            comedySide.hide();
+            trainSide.hide();
+            gullsSide.hide();
+            brewPage.hide();
+        })
+    }
+    duckDive();
+
+    function xy() {
+        var xyClicked = $(".project07");
+
+        xyClicked.on('click', function () {
+            portfolio.hide();
+            pretzelPage.hide();
+            pretzelSide.hide();
+            brewSide.hide();
+            valleySide.hide();
+            alightSide.hide();
+            triviaSide.hide();
+            duckSide.hide();
+            xySide.show();
+            comedySide.hide();
+            trainSide.hide();
+            gullsSide.hide();
+            brewPage.hide();
+        })
+    }
+    xy();
+
+    function comedy() {
+        var comedyClicked = $(".project08");
+
+        comedyClicked.on('click', function () {
+            portfolio.hide();
+            pretzelPage.hide();
+            pretzelSide.hide();
+            brewSide.hide();
+            valleySide.hide();
+            alightSide.hide();
+            triviaSide.hide();
+            duckSide.hide();
+            xySide.hide();
+            comedySide.show();
+            trainSide.hide();
+            gullsSide.hide();
+            brewPage.hide();
+        })
+    }
+    comedy();
+
+    function train() {
+        var trainClicked = $(".project09");
+
+        trainClicked.on('click', function () {
+            portfolio.hide();
+            pretzelPage.hide();
+            pretzelSide.hide();
+            brewSide.hide();
+            valleySide.hide();
+            alightSide.hide();
+            triviaSide.hide();
+            duckSide.hide();
+            xySide.hide();
+            comedySide.hide();
+            trainSide.show();
+            gullsSide.hide();
+            brewPage.hide();
+        })
+    }
+    train();
+
+    function gulls() {
+        var gullsClicked = $(".project10");
+
+        gullsClicked.on('click', function () {
+            portfolio.hide();
+            pretzelPage.hide();
+            pretzelSide.hide();
+            brewSide.hide();
+            valleySide.hide();
+            alightSide.hide();
+            triviaSide.hide();
+            duckSide.hide();
+            xySide.hide();
+            comedySide.hide();
+            trainSide.hide();
+            gullsSide.show();
+            brewPage.hide();
+        })
+    }
+    gulls();
 
 
     // Modal Images for Portfolio Page //
@@ -195,98 +475,98 @@ $(document).ready(function(){
 
     var img01 = document.getElementById('logic-01');
     var img02 = document.getElementById('logic-02');
-    var img03 = document.getElementById('dive-01');
-    var img04 = document.getElementById('view-01');
-    var img05 = document.getElementById('view-02');
-    var img06 = document.getElementById('gulls-01');
-    var img07 = document.getElementById('xy-01');
-    var img08 = document.getElementById('sdba-01');
-    var img09 = document.getElementById('brew');
-    var img10 = document.getElementById('zaldana');
-    var img11 = document.getElementById('alight-01');
-    var img12 = document.getElementById('alight-02');
-    
+    var img03 = document.getElementById('brew-01');
+    // var img04 = document.getElementById('view-01');
+    // var img05 = document.getElementById('view-02');
+    // var img06 = document.getElementById('gulls-01');
+    // var img07 = document.getElementById('xy-01');
+    // var img08 = document.getElementById('sdba-01');
+    // var img09 = document.getElementById('brew');
+    // var img10 = document.getElementById('zaldana');
+    // var img11 = document.getElementById('alight-01');
+    // var img12 = document.getElementById('alight-02');
+
 
     var modalImg = document.getElementById("img01");
     var captionText = document.getElementById("caption");
 
-    img01.onclick = function(){
-      modal.style.display = "block";
-      modalImg.src = this.src;
-      captionText.innerHTML = this.alt;
+    img01.onclick = function () {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
     }
 
-    img02.onclick = function(){
+    img02.onclick = function () {
         modal.style.display = "block";
         modalImg.src = this.src;
         captionText.innerHTML = this.alt;
-      }
+    }
 
-    img03.onclick = function(){
+    img03.onclick = function () {
         modal.style.display = "block";
         modalImg.src = this.src;
         captionText.innerHTML = this.alt;
-      }
+    }
 
-      img04.onclick = function(){
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-      }
+    //   img04.onclick = function(){
+    //     modal.style.display = "block";
+    //     modalImg.src = this.src;
+    //     captionText.innerHTML = this.alt;
+    //   }
 
-      img05.onclick = function(){
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-      }
+    //   img05.onclick = function(){
+    //     modal.style.display = "block";
+    //     modalImg.src = this.src;
+    //     captionText.innerHTML = this.alt;
+    //   }
 
-      img06.onclick = function(){
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-      }
+    //   img06.onclick = function(){
+    //     modal.style.display = "block";
+    //     modalImg.src = this.src;
+    //     captionText.innerHTML = this.alt;
+    //   }
 
-      img07.onclick = function(){
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-      }
+    //   img07.onclick = function(){
+    //     modal.style.display = "block";
+    //     modalImg.src = this.src;
+    //     captionText.innerHTML = this.alt;
+    //   }
 
-      img08.onclick = function(){
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-      }
+    //   img08.onclick = function(){
+    //     modal.style.display = "block";
+    //     modalImg.src = this.src;
+    //     captionText.innerHTML = this.alt;
+    //   }
 
-      img09.onclick = function(){
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-      }
+    //   img09.onclick = function(){
+    //     modal.style.display = "block";
+    //     modalImg.src = this.src;
+    //     captionText.innerHTML = this.alt;
+    //   }
 
-      img10.onclick = function(){
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-      }
+    //   img10.onclick = function(){
+    //     modal.style.display = "block";
+    //     modalImg.src = this.src;
+    //     captionText.innerHTML = this.alt;
+    //   }
 
-      img11.onclick = function(){
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-      }
+    //   img11.onclick = function(){
+    //     modal.style.display = "block";
+    //     modalImg.src = this.src;
+    //     captionText.innerHTML = this.alt;
+    //   }
 
-      img12.onclick = function(){
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-      }
-    
+    //   img12.onclick = function(){
+    //     modal.style.display = "block";
+    //     modalImg.src = this.src;
+    //     captionText.innerHTML = this.alt;
+    //   }
+
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
-    
+
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function() { 
-      modal.style.display = "none";
+    span.onclick = function () {
+        modal.style.display = "none";
     }
 })
